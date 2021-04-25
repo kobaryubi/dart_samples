712d563d326082bfa9ea8e30b1da6e351ab50b77
MasahikoKobayashi <kobaryubi6@gmail.com>
2021-04-25 15:32:04 +0900

    add sample

---
+++ getters_and_setters.dart
@@ -0,0 +1,16 @@
+class Rectangle {
+  double left, top, width, height;
+  Rectangle(this.left, this.top, this.width, this.height);
+
+  double get right => left + width;
+  set right(double value) => left = value - width;
+  double get bottom => top + height;
+  set bottom(double value) => top = value - height;
+}
+
+void main() {
+  var rect = Rectangle(3, 4, 20, 15);
+  assert(rect.left == 3);
+  rect.right = 12;
+  assert(rect.left == -8);
+}
